54a09c9ee259dd5bf9e0a86c204e5694498d08cf
Daniel Conlon <danielcconlon@gmail.com>
2022-07-24 18:06:05 -0400

    login route is working

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -89,6 +89,47 @@ router.put('/:id', (req, res) => {
     });
 });
 
+router.post('/login', (req, res) => {
+  User.findOne({
+    where: {
+      username: req.body.username
+    }
+  })
+  .then(dbUserData => {
+    if(!dbUserData) {
+      res.status(400).json({ message: 'No account with that login' });
+      return;
+    }
+
+    const validPassword = dbUserData.checkPassword(req.body.password);
+
+    if(!validPassword) {
+      res.status(400).json({ message: 'Incorrect password!'});
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
+  
+      res.json({ user: dbUserData, message: 'You are now logged in!' });
+    });
+  });
+});
+
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  }
+  else {
+    res.status(404).end();
+  }
+});
+
+
 router.delete('/:id', (req, res) => {
   User.destroy({
     where: {
@@ -107,5 +148,6 @@ router.delete('/:id', (req, res) => {
       res.status(500).json(err);
     });
 });
+
 
 module.exports = router;
