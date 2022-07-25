7738d549ddf364bdc96753c290bb9299cd7005f2
Daniel Conlon <danielcconlon@gmail.com>
2022-07-24 21:41:15 -0400

    login now works

--- public/js/login.js
+++ public/js/login.js
@@ -1,25 +1,26 @@
-const loginFormHandler = async function(event) {
+async function loginFormHandler(event) {
   event.preventDefault();
 
-  const usernameEl = document.querySelector('#username-login');
-  const passwordEl = document.querySelector('#password-login');
+  const username = document.querySelector('#username-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
 
-  const response = await fetch('/api/users/login', {
-    method: 'POST',
-    body: JSON.stringify({
-      username: usernameEl.value,
-      password: passwordEl.value,
-    }),
-    headers: { 'Content-Type': 'application/json' },
-  });
+  if (username && password) {
+    const response = await fetch('/api/users/login', {
+      method: 'post',
+      body: JSON.stringify({
+        username,
+        password
+      }),
+      headers: { 'Content-Type': 'application/json' }
+    });
 
-  if (response.ok) {
-    document.location.replace('/dashboard');
-  } else {
-    alert('Failed to login');
+    if (response.ok) {
+      document.location.replace('/dashboard/');
+    } else {
+      alert(response.statusText);
+    }
   }
-};
+}
 
-document
-  .querySelector('#login-form')
-  .addEventListener('submit', loginFormHandler);
+
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
