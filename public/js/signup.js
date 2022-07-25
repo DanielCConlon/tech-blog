6220ac84280d0801347dc2cfc6e9c48a1041f5ec
Daniel Conlon <danielcconlon@gmail.com>
2022-07-24 22:18:56 -0400

    signup logic working now

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,25 +1,26 @@
-const signupFormHandler = async function(event) {
+async function signupFormHandler(event) {
   event.preventDefault();
 
-  const usernameEl = document.querySelector('#username-signup');
-  const passwordEl = document.querySelector('#password-signup');
+  const username = document.querySelector('#username-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
 
-  const response = await fetch('/api/user', {
-    method: 'POST',
-    body: JSON.stringify({
-      username: usernameEl.value,
-      password: passwordEl.value,
-    }),
-    headers: { 'Content-Type': 'application/json' },
-  });
+  if (username && password) {
+    const response = await fetch('/api/users', {
+      method: 'post',
+      body: JSON.stringify({
+        username,
+        password
+      }),
+      headers: { 'Content-Type': 'application/json' }
+    });
 
-  if (response.ok) {
-    document.location.replace('/dashboard');
-  } else {
-    alert('Failed to sign up');
+    if (response.ok) {
+      document.location.replace('/');
+    } 
+    else {
+      alert(response.statusText);
+    }
   }
-};
+}
 
-document
-  .querySelector('#signup-form')
-  .addEventListener('submit', signupFormHandler);
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
