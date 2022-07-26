e293b5c0af9de87bd10cbf675d17b20c958d6a0c
Daniel Conlon <danielcconlon@gmail.com>
2022-07-25 22:41:12 -0400

    adding a comment now works

--- public/js/comment.js
+++ public/js/comment.js
@@ -2,17 +2,17 @@ async function commentFormHandler(event) {
   event.preventDefault();
 
   const comment_text = document.querySelector('textarea[name="comment-body"]').value;
-  const postId = window.location.toString().split("/")[
+  const post_id = window.location.toString().split("/")[
     window.location.toString().split("/").length - 1
   ];
 
-  console.log(postId);
+  console.log(post_id);
 
   if (comment_text) {
     const response = await fetch('/api/comments', {
       method: 'POST',
       body: JSON.stringify({
-        postId,
+        post_id,
         comment_text
       }),
       headers: {
